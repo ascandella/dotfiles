657266747cc8a5399f37ed2136690882bd48bfb9
Aiden Scandella <sc@ndella.com>
2015-06-24 13:51:16 -0700

    Add colorization to compilation mode window

--- .spacemacs-private/ui/config.el
+++ .spacemacs-private/ui/config.el
@@ -8,4 +8,14 @@
   (progn
     (setq-default dotspacemacs-default-font '(:size 14))))
 
+; enable colorization in compilation windo
+(require 'ansi-color)
+
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region (point-min) (point-max))
+  (toggle-read-only))
+
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
 ;;; config.el ends here
