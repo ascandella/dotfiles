d5c048b1e388dd8a80099e956b3ed0136d595524
Aiden Scandella <sc@ndella.com>
2015-06-27 12:58:56 -0700

    Colorize shell-command output

--- .spacemacs-private/ui/config.el
+++ .spacemacs-private/ui/config.el
@@ -1,3 +1,4 @@
+(require 'ansi-color)
 (setq-default fill-column 80)
 
 (set-face-attribute 'vertical-border
@@ -17,5 +18,13 @@
   (toggle-read-only))
 
 (add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
+(defadvice display-message-or-buffer (before ansi-color activate)
+  "Process ANSI color codes in shell output."
+  (let ((buf (ad-get-arg 0)))
+    (and (bufferp buf)
+         (string= (buffer-name buf) "*Shell Command Output*")
+         (with-current-buffer buf
+           (ansi-color-apply-on-region (point-min) (point-max))))))
 
 ;;; config.el ends here
