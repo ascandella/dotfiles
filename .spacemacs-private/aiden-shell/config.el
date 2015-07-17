4de66cfd484720054d1e97b20acad429aaee7e54
Aiden Scandella <sc@ndella.com>
2015-07-17 12:18:52 -0700

    do more stuff for shell mode

--- .spacemacs-private/aiden-shell/config.el
+++ .spacemacs-private/aiden-shell/config.el
@@ -1,5 +1,11 @@
 (defun my-bash-mode-hook ()
   (shift-width 2)
-  (setq tab-width 2))
+  (setq
+   tab-width 2
+   sh-basic-offset 2
+   sh-indentation 2
+   sh-basic-indent-line 2)
+  (highlight-indentation-mode)
+  (define-key sh-mode-map (kbd "RET") 'reindent-then-newline-and-indent))
 
 (add-hook 'sh-mode-hook 'my-bash-mode-hook)
