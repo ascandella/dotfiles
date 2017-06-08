97451cb3fa0570a9a7cdaaf1a6959d351d48f70a
Aiden Scandella <git@sca.ndella.com>
2017-06-07 22:49:14 -0700

    Fix print-and-link

--- .support/print-and-link.sh
+++ .support/print-and-link.sh
@@ -1,8 +1,8 @@
- () {
+ _printAndLink () {
   local source="$1"
   local dest="$2"
   echo -e "Linking  ${GRAY_BG}${source}${RESET} -> ${BLUE_BG}${dest}${RESET}"
   ln -s "${source}" "${dest}"
 }
 
-# vim set ft=bash
+# vim: ft=sh
