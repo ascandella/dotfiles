a8b219540bf229f36bcee1cbeb0fcff9c4e2f869
Aiden Scandella <git@sca.ndella.com>
2017-06-20 12:03:13 -0700

    Use 4-space intent-tabs FOR PROFIT

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ set -o pipefail
 # http://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
 pushd "$(dirname "${0}")" > /dev/null
 THISDIR="$(pwd -P)"
+popd > /dev/null
 DOTFILES_DEBUG="${DOTFILES_DEBUG:-}"
 
 SUPPORT="${THISDIR}/.support"
