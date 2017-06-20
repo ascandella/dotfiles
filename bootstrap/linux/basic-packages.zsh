ee38a1c8d56380d528fe3a7c63bfe9618f3afcad
Aiden Scandella <git@sca.ndella.com>
2017-06-20 07:21:09 -0700

    Upgrade basic linux

--- bootstrap/linux/basic-packages.zsh
+++ bootstrap/linux/basic-packages.zsh
@@ -1,1 +1,7 @@
-sudo apt-get install --upgrade -y htop git
+if command -v apt-get >/dev/null ; then
+  export _PKG_INSTALL=(apt-get install --upgrade -y)
+elif command -v dnf >/dev/null ; then
+  export _PKG_INSTALL=(dnf install -y)
+fi
+
+sudo "$_PKG_INSTALL[@]" htop git tmux
