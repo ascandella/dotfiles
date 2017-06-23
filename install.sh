2ed0c4f68d8eae2b5fa8e6178d0e8ee131d274c8
Aiden Scandella <git@sca.ndella.com>
2017-06-22 23:43:43 -0700

    Whoops

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ set -o pipefail
 # http://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
 pushd "$(dirname "${0}")" > /dev/null
 THISDIR="$(pwd -P)"
-popd > /dev/null
 DOTFILES_DEBUG="${DOTFILES_DEBUG:-}"
 
 SUPPORT="${THISDIR}/.support"
@@ -262,3 +261,4 @@ fi
 
 echo
 echo -e "${BOLD}Installation complete${RESET}"
+popd > /dev/null
