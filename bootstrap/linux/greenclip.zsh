60223822bb33f53169a178051ca3793d690d3210
Aiden Scandella <git@sca.ndella.com>
2018-02-12 10:47:43 -0800

    Use greenclip in i3

--- bootstrap/linux/greenclip.zsh
+++ bootstrap/linux/greenclip.zsh
@@ -1,4 +1,4 @@
 if ! command -v greenclip >/dev/null ; then
-  wget https://github.com/erebe/greenclip/releases/download/2.1/greenclip -o ~/bin/greenclip
+  wget https://github.com/erebe/greenclip/releases/download/2.1/greenclip -O ~/bin/greenclip
   chmod +x ~/bin/greenclip
 fi
