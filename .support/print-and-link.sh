feb9617a9a9e4d212bfe31c2001562b296f85225
Aiden Scandella <git@sca.ndella.com>
2018-03-14 17:35:06 -0700

    Create enclosing directory for print-and-link

--- .support/print-and-link.sh
+++ .support/print-and-link.sh
@@ -2,6 +2,11 @@
   local source="$1"
   local dest="$2"
   echo -e "Linking  ${GRAY_BG}${source}${RESET} -> ${BLUE_BG}${dest}${RESET}"
+  local destdir="$(dirname "${dest}")"
+  if [[ ! -d "${destdir}" ]]; then
+    echo -e "  Creating parent directory ${BLUE_BG}${destdir}${RESET}"
+    mkdir -p "${destdir}"
+  fi
   ln -s "${source}" "${dest}"
 }
 
