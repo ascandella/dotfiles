c5a563e6ef8260ad56176337882c4fd01930c38c
Aiden Scandella <git@sca.ndella.com>
2018-07-17 08:25:17 -0700

    Move polybar to builtin display on Thinkpad

--- to-install/linux/dotconfig/polybar/launch.sh
+++ to-install/linux/dotconfig/polybar/launch.sh
@@ -7,6 +7,12 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar ai &
+MONITOR=""
+# If we're on my Thinkpad, pin Polybar to the builtin display, as opposed to the
+# (possibly-connected) "primary monitor"
+if [[ "$(cat /sys/class/drm/card0/card0-eDP-1/status)" == "connected" ]] ; then
+  MONITOR="eDP-1"
+fi
+MONITOR="${MONITOR}" polybar ai &
 
 echo "Bars launched..."
