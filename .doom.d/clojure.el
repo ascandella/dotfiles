475595eadfc37217d00eab7125793f37658558fd
Aiden Scandella <git@sca.ndella.com>
2020-05-01 17:44:51 -0700

    Disable parinfer by default in Clojure to avoid formatting clashes

--- .doom.d/clojure.el
+++ .doom.d/clojure.el
@@ -34,7 +34,7 @@
             paredit        ; Introduce some paredit commands.
             smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
             smart-yank))   ; Yank behavior depend on mode.
-    (add-hook 'clojure-mode-hook #'parinfer-mode)
+    ;(add-hook 'clojure-mode-hook #'parinfer-mode)
     (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
     (add-hook 'common-lisp-mode-hook #'parinfer-mode)
     (add-hook 'scheme-mode-hook #'parinfer-mode)
