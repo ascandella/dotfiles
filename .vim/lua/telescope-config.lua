afe1bdf4972763754e97c05f6f9350e871f5ed9e
Aiden Scandella <git@sca.ndella.com>
2021-05-26 19:24:08 -0700

    Fix telescope project_files ,, mapping

--- .vim/lua/telescope-config.lua
+++ .vim/lua/telescope-config.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.project_files = function()
-  local opts = {} -- define here if you want to define something
+  local opts = require('telescope.themes').get_dropdown({  winblend = 10 })
   local ok = pcall(require'telescope.builtin'.git_files, opts)
   if not ok then require'telescope.builtin'.find_files(opts) end
 end
