a55d8b104b4dad017ea57afdeb86f048370b856d
Aiden Scandella <git@sca.ndella.com>
2021-06-03 09:43:24 -0700

    Add elixir ,ee tests with floatterm

--- .vim/init/pgp.vim
+++
@@ -1,6 +0,0 @@
-"
-" PGP
-"
-nnoremap <silent> <leader>ee :w !gpg --decrypt; wait<cr>
-nnoremap <silent> <leader>ec :w !gpg --encrypt --armor --trust-model direct -o- -r
-nnoremap <silent> <leader>ei :w !gpg --import<cr>
--- .vim/lua/ai/elixir-config.lua
+++ .vim/lua/ai/elixir-config.lua
@@ -1,4 +1,6 @@
-require('lspconfig').elixirls.setup{
+local nvim_lsp = require('lspconfig')
+
+nvim_lsp.elixirls.setup{
   on_attach = require('ai/lsp-shared').on_attach,
   cmd = {vim.fn.expand("$HOME/src/elixir-ls/language_server.sh")}
 }
@@ -6,3 +8,34 @@ require('lspconfig').elixirls.setup{
 vim.api.nvim_command([[
   autocmd BufEnter *.ex :setlocal filetype=elixir
 ]])
+
+vim.api.nvim_exec([[
+  augroup AiElixir
+    autocmd!
+    autocmd FileType elixir nnoremap <silent><buffer> <leader>ee :lua require('ai/elixir-config').run_tests() <cr>
+  augroup END
+]], false)
+
+local M = {}
+local buffer_opened = false
+
+local project_root_finder = nvim_lsp.util.root_pattern('mix.exs', '.git')
+
+M.open_tests = function()
+  local buffer_filename = vim.api.nvim_buf_get_name(0)
+  local project_root = project_root_finder(buffer_filename)
+  vim.api.nvim_command([[FloatermNew --name=elixir --position=bottomright --cwd=]] .. project_root)
+  buffer_opened = true
+end
+
+M.run_tests = function()
+  if not buffer_opened then
+    M.open_tests()
+  end
+  vim.api.nvim_exec([[
+    FloatermSend --name=elixir mix test --stale
+    FloatermShow --name=elixir
+  ]], false)
+end
+
+return M
