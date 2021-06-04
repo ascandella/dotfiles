ba54c96aa4ea8c5c959014a8bd7468195c179e44
Aiden Scandella <git@sca.ndella.com>
2021-06-04 07:45:44 -0700

    Minor elixir refactor

--- .vim/lua/ai/elixir-commands.lua
+++ .vim/lua/ai/elixir-commands.lua
@@ -14,12 +14,18 @@ local function open_tests()
 end
 
 local function send_command_and_show(command)
+  if not buffer_opened then
+    open_tests()
+  end
+
   local full_command = [[FloatermSend --name=elixir ]] .. command
   local resp = vim.api.nvim_exec(full_command, true)
+
   if string.match(resp, ".*No more floaterms.*") then
     open_tests()
     vim.api.nvim_command(full_command)
   end
+
   vim.api.nvim_command([[FloatermShow --name=elixir]])
 end
 
@@ -29,19 +35,12 @@ local function current_line_number()
 end
 
 M.run_tests = function()
-  if not buffer_opened then
-    open_tests()
-  end
   send_command_and_show('mix test --stale')
 end
 
 M.run_test_at_cursor = function()
   local buffer_filename = vim.api.nvim_buf_get_name(0)
   local buffer_lineno = current_line_number()
-
-  if not buffer_opened then
-    open_tests()
-  end
 
   send_command_and_show('mix test ' .. buffer_filename .. ':' .. buffer_lineno)
 end
