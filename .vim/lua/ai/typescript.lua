aa6dd212cb45ad83893691ffa8ac6d493c77b5fc
Aiden Scandella <git@sca.ndella.com>
2021-06-10 15:03:39 -0700

    Always enable signcolumn on TS/TSX for LSP and gitgutter

--- .vim/lua/ai/typescript.lua
+++ .vim/lua/ai/typescript.lua
@@ -64,7 +64,7 @@ nvim_lsp.diagnosticls.setup {
 nvim_lsp.tsserver.setup {
   capabilities = require('ai/lsp-shared').capabilities(),
   on_attach = function(client)
-    client.resolved_capabilities.document_formatting = true
+    client.resolved_capabilities.document_formatting = false
     lsp_shared.on_attach(client)
   end
 }
