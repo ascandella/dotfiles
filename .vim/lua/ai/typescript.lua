21522c053d93dbf5aafed84b0dfce0539c06de76
Aiden Scandella <git@sca.ndella.com>
2021-06-21 11:49:57 -0700

    Re-add signcolumn stuff for typescript (lost when removing diagnosticls)

--- .vim/lua/ai/typescript.lua
+++ .vim/lua/ai/typescript.lua
@@ -15,6 +15,7 @@ nvim_lsp.tsserver.setup {
   capabilities = require('ai/lsp-shared').capabilities(),
   on_attach = function(client)
     client.resolved_capabilities.document_formatting = false
+    vim.api.nvim_exec([[set signcolumn=yes]], true)
     lsp_shared.on_attach(client)
   end,
 }
