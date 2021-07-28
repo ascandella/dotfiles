d74b051404363b5d8e9063d5dcb2cbcb281d7a49
Aiden Scandella <git@sca.ndella.com>
2021-07-28 16:03:08 -0700

    Add stylua project config and search parent directories

--- .vim/lua/ai/_autopairs.lua
+++ .vim/lua/ai/_autopairs.lua
@@ -2,10 +2,10 @@ local npairs = require('nvim-autopairs')
 local Rule = require('nvim-autopairs.rule')
 
 npairs.setup({
-  disable_filetype = { "TelescopePrompt" , "frecency", "vim" },
+  disable_filetype = { 'TelescopePrompt', 'frecency', 'vim' },
   check_ts = true,
 })
 
 npairs.add_rules({
-  Rule('"""', '"""', { "elixir", "eelixir" }),
+  Rule('"""', '"""', { 'elixir', 'eelixir' }),
 })
