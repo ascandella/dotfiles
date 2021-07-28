7e6e6232a2aff92582a38095dbf14b7e2e056cad
Aiden Scandella <git@sca.ndella.com>
2021-07-28 16:14:35 -0700

    Add harpoon mappings and stylua the file

--- .vim/lua/ai/keymappings.lua
+++ .vim/lua/ai/keymappings.lua
@@ -1,44 +1,47 @@
-local k = require("astronauta.keymap")
+local k = require('astronauta.keymap')
 local nnoremap = k.nnoremap
 local tnoremap = k.tnoremap
 local telescope_commands = require('ai/telescope-config')
 
-nnoremap { '-', '<cmd>Explore<cr>', { silent = true } }
-nnoremap { '<Leader>,', telescope_commands.project_files, { silent = true } }
+nnoremap({ '-', '<cmd>Explore<cr>', { silent = true } })
+nnoremap({ '<Leader>,', telescope_commands.project_files, { silent = true } })
 
-nnoremap { '<Leader>h', telescope_commands.frecency, { silent = true } }
+nnoremap({ '<Leader>h', telescope_commands.frecency, { silent = true } })
 
-nnoremap { '<Leader>n', telescope_commands.buffers, { silent = true } }
-nnoremap {
+nnoremap({ '<Leader>n', telescope_commands.buffers, { silent = true } })
+nnoremap({
   '<Leader>gf',
   require('telescope.builtin').git_status,
   { silent = true },
-}
+})
 
-nnoremap { '<Leader>a', telescope_commands.livegrep_project, { silent = true } }
+nnoremap({ '<Leader>a', telescope_commands.livegrep_project, { silent = true } })
 
-nnoremap {
+nnoremap({
   '<Leader>fs',
   telescope_commands.grep_string_hidden,
   { silent = true },
-}
-nnoremap { '<Leader>fb', telescope_commands.git_branches, { silent = true } }
+})
+nnoremap({ '<Leader>fb', telescope_commands.git_branches, { silent = true } })
 
-nnoremap { '<Leader>S', require('spectre').open, { silent = true } }
+nnoremap({ '<Leader>S', require('spectre').open, { silent = true } })
 
-nnoremap { '<Leader>sw', require('spectre').open_visual, { silent = true } }
+nnoremap({ '<Leader>sw', require('spectre').open_visual, { silent = true } })
 
-nnoremap { '<Leader>sp', require('spectre').open_file_search, { silent = true } }
+nnoremap({ '<Leader>sp', require('spectre').open_file_search, { silent = true } })
 
-nnoremap { '<Leader>gg', '<cmd>Neogit<cr>', { silent = true } }
+nnoremap({ '<Leader>gg', '<cmd>Neogit<cr>', { silent = true } })
 
-nnoremap { '<Leader>u', '<cmd>UndotreeToggle<cr>', { silent = true } }
+nnoremap({ '<Leader>u', '<cmd>UndotreeToggle<cr>', { silent = true } })
 
-nnoremap { '<Leader>ft', '<cmd>FloatermToggle<cr>', { silent = true } }
-nnoremap { '<A-j>', '<cmd>FloatermToggle<cr>', { silent = true } }
+nnoremap({ '<A-h>', require('harpoon.ui').toggle_quick_menu, { silent = true } })
+nnoremap({ '<Leader>m', require('harpoon.mark').add_file, { silent = true } })
+
+nnoremap({ '<Leader>ft', '<cmd>FloatermToggle<cr>', { silent = true } })
+nnoremap({ '<A-j>', '<cmd>FloatermToggle<cr>', { silent = true } })
 
 -- Terminal mode mappings
-tnoremap { '<Leader>ft', '<cmd>FloatermToggle<cr>', { silent = true } }
-tnoremap { '<A-j>', '<cmd>FloatermToggle<cr>', { silent = true } }
+tnoremap({ '<Leader>ft', '<cmd>FloatermToggle<cr>', { silent = true } })
+tnoremap({ '<A-j>', '<cmd>FloatermToggle<cr>', { silent = true } })
 -- Go back to normal mode
-tnoremap { '<Esc>', '<C-\\><C-n>', { silent = true } }
+tnoremap({ '<Esc>', '<C-\\><C-n>', { silent = true } })
