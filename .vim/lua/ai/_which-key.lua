4174a4c79712c179e99a0a7b6d43cdc87d75eb86
Aiden Scandella <git@sca.ndella.com>
2021-08-04 14:01:57 -0700

    Format and add to which-key

--- .vim/lua/ai/_which-key.lua
+++ .vim/lua/ai/_which-key.lua
@@ -1,67 +1,71 @@
 -- From:
 -- https://github.com/elianiva/dotfiles/blob/master/nvim/lua/plugins/_which-key.lua
-local wk = require("which-key")
+local wk = require('which-key')
 
 local registrations = {
-  b = "Close buffer",
-  h = "Telescope Frecency",
+  b = 'Close buffer',
+  h = 'Telescope Frecency',
   f = {
-    name = "+Telescope",
-    b    = "Git branches",
-    f    = "Files",
-    h    = "Help",
-    g    = "Live grep",
-    l    = "File Browser",
-    s    = "Search word under cursor",
+    name = '+Telescope',
+    b = 'Git branches',
+    f = 'Files',
+    h = 'Help',
+    g = 'Live grep',
+    l = 'File Browser',
+    s = 'Search word under cursor',
   },
   g = {
-    name  = "+LSP",
-    a     = "Code Action" ,
-    d     = "Symbol Definition(s)" ,
-    r     = "Symbol Reference(s)" ,
-    R     = "Rename Symbol" ,
-    t     = "Show Diagnostic List" ,
-    ["]"] = "Next Diagnostic",
-    ["["] = "Prev Diagnostic" ,
+    name = '+LSP',
+    a = 'Code Action',
+    c = 'Create Pull Request',
+    d = 'Symbol Definition(s)',
+    r = 'Symbol Reference(s)',
+    R = 'Rename Symbol',
+    t = 'Show Diagnostic List',
+    f = 'Git Files Telescope',
+    n = 'Next Hunk',
+    m = 'Git Messenger Line',
+    [']'] = 'Next Diagnostic',
+    ['['] = 'Prev Diagnostic',
   },
-  n = "Telescope buffers",
-  a = "Swap With Next Parameter",
-  A = "Swap With Prev Parameter",
-  w = "Write",
-  S = "Search and replace",
+  n = 'Telescope buffers',
+  a = 'Swap With Next Parameter',
+  A = 'Swap With Prev Parameter',
+  w = 'Write',
+  S = 'Search and replace',
   s = {
-    s = "Sort paragraph",
-    w = "Search and replace word under cursor",
+    s = 'Sort paragraph',
+    w = 'Search and replace word under cursor',
   },
   t = {
-    name = "+Toggle",
-    n = "Quickfix",
-    l = "Location List",
-    s = "Toggle trailing whitespace cleanup hook",
+    name = '+Toggle',
+    n = 'Quickfix',
+    l = 'Location List',
+    s = 'Toggle trailing whitespace cleanup hook',
   },
-  u = "Toggle undo tree",
+  u = 'Toggle undo tree',
   y = {
-    name = "+Yank",
-    p = "Current buffer"
+    name = '+Yank',
+    p = 'Current buffer',
   },
 }
-registrations[","] = "Search Project"
-registrations["."] = "Newline above"
-registrations["-"] = "Toggle line comment"
-wk.register(registrations, { prefix = "<leader>" })
+registrations[','] = 'Search Project'
+registrations['.'] = 'Newline above'
+registrations['-'] = 'Toggle line comment'
+wk.register(registrations, { prefix = '<leader>' })
 
 wk.register({
-  gc = "Comments",
-  gJ = "Join Multiline",
-  gS = "Split Into Multiline",
+  gc = 'Comments',
+  gJ = 'Join Multiline',
+  gS = 'Split Into Multiline',
 
   -- vim-sandwich
-  sa = "Add Surrounding Character",
-  sd = "Remove Surrounding Character",
-  sr = "Replace Surrounding Character",
+  sa = 'Add Surrounding Character',
+  sd = 'Remove Surrounding Character',
+  sr = 'Replace Surrounding Character',
 })
 
-wk.setup {
+wk.setup({
   plugins = {
     marks = true,
     registers = true,
@@ -77,16 +81,16 @@ wk.setup {
   },
   -- add operators that will trigger motion and text object completion
   -- to enable all native operators, set the preset / operators plugin above
-  operators = { gc = "Comments" },
+  operators = { gc = 'Comments' },
   icons = {
-    breadcrumb = "»",
-    separator = "➜ ",
-    group = "+",
+    breadcrumb = '»',
+    separator = '➜ ',
+    group = '+',
   },
   window = {
     -- border = { "", "▔", "", "", "", "", "", "" }, -- none, single, double, shadow
-    border = "none",
-    position = "bottom", -- bottom, top
+    border = 'none',
+    position = 'bottom', -- bottom, top
     margin = { 0, 0, 0, 0 }, -- extra window margin [top, right, bottom, left]
     padding = { 4, 2, 4, 2 }, -- extra window padding [top, right, bottom, left]
   },
@@ -97,15 +101,15 @@ wk.setup {
   },
   ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
   hidden = {
-    "<silent>",
-    "<cmd>",
-    "<Cmd>",
-    "<CR>",
-    "call",
-    "lua",
-    "^:",
-    "^ ",
+    '<silent>',
+    '<cmd>',
+    '<Cmd>',
+    '<CR>',
+    'call',
+    'lua',
+    '^:',
+    '^ ',
   }, -- hide mapping boilerplate
   show_help = true, -- show help message on the command line when the popup is visible
-  triggers = "auto", -- automatically setup triggers
-}
+  triggers = 'auto', -- automatically setup triggers
+})
