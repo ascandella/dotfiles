b861b5be2543b153734cfbc8499bebef9bfe91a0
Aiden Scandella <git@sca.ndella.com>
2021-08-16 12:14:58 -0700

    Improve opening pull requests

--- .vim/lua/ai/_neogit.lua
+++ .vim/lua/ai/_neogit.lua
@@ -15,14 +15,17 @@ local M = {}
 M.open_pr = function()
   a.scope(function()
     local remote_url = a.await(neogit.cli.config.get('remote.origin.url').call())[1]
-    local repo_name = remote_url:sub(16)
+    local repo_name = remote_url:gsub('^git@github.com:', '')
+    local clean_name = repo_name:gsub('.git$', '')
     local branch = status.repo.head.branch
 
-    local open_pr_url = string.format('https://github.com/%s/pull/new/%s', repo_name, branch)
+    local open_pr_url = string.format('https://github.com/%s/pull/new/%s', clean_name, branch)
     a.await(a.scheduler())
 
     if vim.fn.has('mac') == 1 then
       vim.cmd('!open ' .. open_pr_url)
+    elseif vim.fn.executable('xdg-open') == 1 then
+      vim.cmd('silent !xdg-open ' .. open_pr_url)
     else
       -- Fall back to copying to clipboard (e.g. for WSL)
       vim.cmd('let @+="' .. open_pr_url .. '"')
