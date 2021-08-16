a885b295eff29dbb6d9077fe9e818b6ecaa37f7b
Aiden Scandella <git@sca.ndella.com>
2021-08-16 09:38:40 -0700

    Only set up local sumneko when present; use lspinstall

--- .vim/lua/ai/lua-ls.lua
+++ .vim/lua/ai/lua-ls.lua
@@ -13,30 +13,32 @@ else
     print("Unsupported system for sumneko")
 end
 
-require('lspconfig').sumneko_lua.setup {
-  on_attach = require('ai/lsp-shared').on_attach,
-  capabilities = require('ai/lsp-shared').capabilities(),
-  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        path = vim.split(package.path, ';')
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'}
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true},
-        maxPreload = 10000,
-        ignoreDir = {
-          vim.o.undodir,
+if vim.fn.isdirectory(sumneko_root_path) ~= 0 then
+  require('lspconfig').sumneko_lua.setup {
+    on_attach = require('ai/lsp-shared').on_attach,
+    capabilities = require('ai/lsp-shared').capabilities(),
+    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+          -- Setup your lua path
+          path = vim.split(package.path, ';')
         },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'}
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true},
+          maxPreload = 10000,
+          ignoreDir = {
+            vim.o.undodir,
+          },
+        }
       }
     }
   }
-}
+end
