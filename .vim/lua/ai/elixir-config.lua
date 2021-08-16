6b054fa6194229bfd2915cc7966288e800c8095c
Aiden Scandella <git@sca.ndella.com>
2021-08-16 09:53:14 -0700

    Use lspinstall for elixirls

--- .vim/lua/ai/elixir-config.lua
+++ .vim/lua/ai/elixir-config.lua
@@ -1,24 +1,28 @@
 local nvim_lsp = require('lspconfig')
+local servers = require('lspinstall').installed_servers()
 
--- LuaFormatter off
-nvim_lsp.elixirls.setup({
-  -- Override to disable eelixir
-  filetypes = { 'elixir '},
-  on_attach = require('ai/lsp-shared').on_attach,
-  capabilities = require('ai/lsp-shared').capabilities(),
-  cmd = { vim.fn.expand("$HOME/src/elixir-ls/language_server.sh") },
-})
+if servers['elixirls'] ~= nil then
+  nvim_lsp.elixirls.setup({
+    -- Override to disable eelixir
+    filetypes = { 'elixir ' },
+    on_attach = require('ai/lsp-shared').on_attach,
+    capabilities = require('ai/lsp-shared').capabilities(),
+    cmd = { vim.fn.expand('$HOME/src/elixir-ls/language_server.sh') },
+  })
+end
 
 vim.api.nvim_command([[
   autocmd BufEnter *.ex :setlocal filetype=elixir
 ]])
 
-vim.api.nvim_exec([[
+vim.api.nvim_exec(
+  [[
   augroup AiElixir
     autocmd!
     autocmd FileType elixir nnoremap <silent><buffer> <leader>ee :lua require('ai/elixir-commands').run_tests() <cr>
     autocmd FileType elixir nnoremap <silent><buffer> <leader>eu :lua require('ai/elixir-commands').run_test_at_cursor() <cr>
     autocmd FileType elixir nnoremap <silent><buffer> <leader>eh :lua require('ai/elixir-commands').run_test_at_file() <cr>
   augroup END
-]], false)
--- LuaFormatter on
+]],
+  false
+)
