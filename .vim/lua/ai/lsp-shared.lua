8694c23d101fe173c98b7e7b6297c3da3336da47
Aiden Scandella <git@sca.ndella.com>
2021-08-20 10:10:29 -0700

    Add lsp telescope, format, and shellfmt

--- .vim/lua/ai/lsp-shared.lua
+++ .vim/lua/ai/lsp-shared.lua
@@ -10,92 +10,79 @@ end
 M.toggle_lsp_formatting = function()
   vim.b.lsp_disable_formatting = not vim.b.lsp_disable_formatting
   if vim.b.lsp_disable_formatting then
-    print("LSP Formatting disabled")
+    print('LSP Formatting disabled')
   else
-    print("LSP Formatting enabled")
+    print('LSP Formatting enabled')
   end
 end
 
 -- LuaFormatter off
 M.on_attach = function(client, bufnr)
   local buf_map = vim.api.nvim_buf_set_keymap
-  vim.cmd("command! LspDef lua vim.lsp.buf.definition()")
+  vim.cmd('command! LspDef lua vim.lsp.buf.definition()')
   vim.cmd("command! LspFormatting lua require('ai/lsp-shared').maybe_lsp_format()")
   vim.cmd("command! LspToggleFormatting lua require('ai/lsp-shared').toggle_lsp_formatting()")
-  vim.cmd("command! LspCodeAction lua vim.lsp.buf.code_action()")
-  vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
-  vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
-  vim.cmd("command! LspOrganize lua lsp_organize_imports()")
-  vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
-  vim.cmd("command! LspTypeDef lua vim.lsp.buf.type_definition()")
-  vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
-  vim.cmd("command! LspDiagPrev lua vim.lsp.diagnostic.goto_prev()")
-  vim.cmd("command! LspDiagNext lua vim.lsp.diagnostic.goto_next()")
-  vim.cmd("command! LspDiagLine lua vim.lsp.diagnostic.show_line_diagnostics()")
-  vim.cmd("command! LspSignatureHelp lua vim.lsp.buf.signature_help()")
+  vim.cmd('command! LspCodeAction lua vim.lsp.buf.code_action()')
+  vim.cmd('command! LspHover lua vim.lsp.buf.hover()')
+  vim.cmd('command! LspRename lua vim.lsp.buf.rename()')
+  vim.cmd('command! LspOrganize lua lsp_organize_imports()')
+  vim.cmd('command! LspRefs lua vim.lsp.buf.references()')
+  vim.cmd('command! LspTypeDef lua vim.lsp.buf.type_definition()')
+  vim.cmd('command! LspImplementation lua vim.lsp.buf.implementation()')
+  vim.cmd('command! LspDiagPrev lua vim.lsp.diagnostic.goto_prev()')
+  vim.cmd('command! LspDiagNext lua vim.lsp.diagnostic.goto_next()')
+  vim.cmd('command! LspDiagLine lua vim.lsp.diagnostic.show_line_diagnostics()')
+  vim.cmd('command! LspSignatureHelp lua vim.lsp.buf.signature_help()')
 
-  buf_map(bufnr, "n", "<C-c><C-j>", ":LspDef<CR>", { silent = true })
-  buf_map(bufnr, "n", "gd", ":LspDef<CR>", { silent = true })
-  buf_map(bufnr, "n", "gr", ":Lspsaga rename<CR>", { silent = true })
-  buf_map(bufnr, "n", "gR", ":Telescope lsp_references<CR>", { silent = true })
-  buf_map(bufnr, "n", "gy", ":LspTypeDef<CR>", { silent = true })
-  buf_map(bufnr, "n", "K", ":Lspsaga hover_doc<CR>", { silent = true })
-  buf_map(bufnr, "n", "gs", ":LspOrganize<CR>", { silent = true })
-  buf_map(
-      bufnr, "n", "[a", ":Lspsaga diagnostic_jump_prev<CR>", { silent = true }
-  )
-  buf_map(
-      bufnr, "n", "]a", ":Lspsaga diagnostic_jump_next<CR>", { silent = true }
-  )
-  buf_map(bufnr, "n", "ga", ":Lspsaga code_action<CR>", { silent = true })
-  buf_map(
-      bufnr, "n", "<Leader>gd", ":Lspsaga preview_definition<CR>",
-      { silent = true }
-  )
-  buf_map(
-      bufnr, "n", "<Leader>d", ":Lspsaga show_line_diagnostics<CR>",
-      { silent = true }
-  )
-  buf_map(
-      bufnr, "i", "<C-x><C-x>", "<cmd> Lspsaga signature_help<CR>",
-      { silent = true }
-  )
-  buf_map(
-      bufnr, "n", "<Leader>tf", ":LspToggleFormatting<CR>",
-      { silent = true }
-  )
+  buf_map(bufnr, 'n', '<C-c><C-j>', ':LspDef<CR>', { silent = true })
+  buf_map(bufnr, 'n', 'gd', ':LspDef<CR>', { silent = true })
+  buf_map(bufnr, 'n', 'gr', ':Lspsaga rename<CR>', { silent = true })
+  buf_map(bufnr, 'n', 'gR', ':Telescope lsp_references<CR>', { silent = true })
+  buf_map(bufnr, 'n', 'gy', ':LspTypeDef<CR>', { silent = true })
+  buf_map(bufnr, 'n', 'K', ':Lspsaga hover_doc<CR>', { silent = true })
+  buf_map(bufnr, 'n', 'gs', ':LspOrganize<CR>', { silent = true })
+  buf_map(bufnr, 'n', '[a', ':Lspsaga diagnostic_jump_prev<CR>', { silent = true })
+  buf_map(bufnr, 'n', ']a', ':Lspsaga diagnostic_jump_next<CR>', { silent = true })
+  buf_map(bufnr, 'n', 'ga', ':Lspsaga code_action<CR>', { silent = true })
+  buf_map(bufnr, 'n', '<Leader>gd', ':Lspsaga preview_definition<CR>', { silent = true })
+  buf_map(bufnr, 'n', '<Leader>d', ':Lspsaga show_line_diagnostics<CR>', { silent = true })
+  buf_map(bufnr, 'n', '<Leader>ld', ':Telescope lsp_document_symbols<CR>', { silent = true })
+  buf_map(bufnr, 'n', '<Leader>lr', ':Telescope lsp_references<CR>', { silent = true })
+  buf_map(bufnr, 'i', '<C-x><C-x>', '<cmd> Lspsaga signature_help<CR>', { silent = true })
+  buf_map(bufnr, 'n', '<Leader>tf', ':LspToggleFormatting<CR>', { silent = true })
 
   -- Illuminate visual display. Visual or Cursorline are good fits
-  vim.api.nvim_command [[ hi def link LspReferenceText CursorLine ]]
-  vim.api.nvim_command [[ hi def link LspReferenceWrite CursorLine ]]
-  vim.api.nvim_command [[ hi def link LspReferenceRead Visual ]]
+  vim.api.nvim_command([[ hi def link LspReferenceText CursorLine ]])
+  vim.api.nvim_command([[ hi def link LspReferenceWrite CursorLine ]])
+  vim.api.nvim_command([[ hi def link LspReferenceRead Visual ]])
 
   -- Illuminate mappings
+  buf_map(bufnr, 'n', '<a-n>', '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>', { noremap = true })
   buf_map(
-      bufnr, 'n', '<a-n>',
-      '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>',
-      { noremap = true }
-  )
-  buf_map(
-      bufnr, 'n', '<a-p>',
-      '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>',
-      { noremap = true }
+    bufnr,
+    'n',
+    '<a-p>',
+    '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>',
+    { noremap = true }
   )
 
   if client.resolved_capabilities.document_formatting then
-    vim.api.nvim_exec([[
+    vim.api.nvim_exec(
+      [[
      augroup LspAutocommands
        autocmd! * <buffer>
        autocmd BufWritePost <buffer> LspFormatting
      augroup END
-     ]], true)
+     ]],
+      true
+    )
   end
 
   require('lsp_signature').on_attach()
   require('illuminate').on_attach(client)
 
   if client.server_capabilities.colorProvider then
-    require("ai/lsp-documentcolors").buf_attach(bufnr, { single_column = true })
+    require('ai/lsp-documentcolors').buf_attach(bufnr, { single_column = true })
   end
 end
 -- LuaFormatter on
--- .zshenv
+++ .zshenv
@@ -1,16 +1,16 @@
 # this file exists so we can use alias in `:!` commands in vim.
 # obviously it will only work for aliases defined in the following file
 
-_sourceIfPresent () {
-  if [[ -z "${1}" ]] ; then
+_sourceIfPresent() {
+  if [[ -z ${1} ]]; then
     echo "Need argument to _sourceIfPresent"
-  elif [[ -f "${1}" ]] ; then
+  elif [[ -f ${1} ]]; then
     # shellcheck disable=SC1090
     . "${1}"
   fi
 }
 
-for halp in "aliases lemonade" ; do
+for halp in "aliases lemonade"; do
   _sourceIfPresent "${DOTFILES:-$HOME/.dotfiles}/shell/${halp}"
 done
 
