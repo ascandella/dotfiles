3b5086092afe94f63061aed4f167eb565b6b7e25
Aiden Scandella <git@sca.ndella.com>
2021-10-20 11:34:05 -0700

    Use updated LSP async formatting handler to sync async changes

--- .vim/lua/ai/lsp-shared.lua
+++ .vim/lua/ai/lsp-shared.lua
@@ -101,4 +101,21 @@ M.capabilities = function()
   return cmp_nvim_lsp.update_capabilities(capabilities)
 end
 
+-- https://jose-elias-alvarez.medium.com/configuring-neovims-lsp-client-for-typescript-development-5789d58ea9c
+local format_async = function(err, _, result, _, bufnr)
+  if err ~= nil or result == nil then
+    return
+  end
+  if not vim.api.nvim_buf_get_option(bufnr, 'modified') then
+    local view = vim.fn.winsaveview()
+    vim.lsp.util.apply_text_edits(result, bufnr)
+    vim.fn.winrestview(view)
+    if bufnr == vim.api.nvim_get_current_buf() then
+      vim.api.nvim_command('noautocmd :update')
+    end
+  end
+end
+
+vim.lsp.handlers['textDocument/formatting'] = format_async
+
 return M
