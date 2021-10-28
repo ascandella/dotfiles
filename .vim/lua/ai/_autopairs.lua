86a2bfaa725fa11f7990087c539a8d78e3d6f1de
Aiden Scandella <git@sca.ndella.com>
2021-10-28 10:12:04 -0700

    Fix endwise lua / elixir and deprecation

--- .vim/lua/ai/_autopairs.lua
+++ .vim/lua/ai/_autopairs.lua
@@ -1,21 +1,18 @@
 local npairs = require('nvim-autopairs')
 local Rule = require('nvim-autopairs.rule')
 
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+
 npairs.setup({
   disable_filetype = { 'TelescopePrompt', 'frecency', 'vim' },
   check_ts = true,
 })
 
-require('nvim-autopairs.completion.cmp').setup({
-  map_cr = true, --  map <CR> on insert mode
-  map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
-  auto_select = false, -- *don't* automatically select the first item
-  insert = true, -- use insert confirm behavior instead of replace
-  map_char = { -- modifies the function or method delimiter by filetypes
-    all = '(',
-  },
-})
-
 npairs.add_rules({
   Rule('"""', '"""', { 'elixir', 'eelixir' }),
 })
+
+npairs.add_rules(require('nvim-autopairs.rules.endwise-elixir'))
+npairs.add_rules(require('nvim-autopairs.rules.endwise-lua'))
