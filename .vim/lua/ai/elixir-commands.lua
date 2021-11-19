8e182722538984997f3ee12370b2d59bd7f65a2e
Aiden Scandella <git@sca.ndella.com>
2021-11-19 08:49:00 -0800

    Search backwards for defmodule instead

--- .vim/lua/ai/elixir-commands.lua
+++ .vim/lua/ai/elixir-commands.lua
@@ -52,7 +52,7 @@ end
 
 M.copy_module_alias = function()
   vim.api.nvim_feedkeys(
-    vim.api.nvim_replace_termcodes([[mTgg0w"zyiW`T<c-w>poalias <c-r>z]], true, false, true),
+    vim.api.nvim_replace_termcodes([[mT?defmodule <cr>w"zyiW`T<c-w>poalias <c-r>z]], true, false, true),
     'n',
     true
   )
