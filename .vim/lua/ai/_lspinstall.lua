ea537a2489eb5692359790140f88770d99c053fb
Aiden Scandella <git@sca.ndella.com>
2021-12-09 09:07:04 -0800

    Add back efm and disable tsserver formatting

--- .vim/lua/ai/_lspinstall.lua
+++ .vim/lua/ai/_lspinstall.lua
@@ -1,19 +1,20 @@
 local cmp_nvim_lsp = require('cmp_nvim_lsp')
-local lsp_installer = require("nvim-lsp-installer")
+local lsp_installer = require('nvim-lsp-installer')
 
 -- Include the servers you want to have installed by default below
 local servers = {
-  "bashls",
-  "elixirls",
-  "gopls",
-  "terraformls",
-  "tailwindcss",
-  "tflint",
-  "tsserver",
-  "pyright",
-  "sumneko_lua",
-  "yamlls",
-  "rust_analyzer",
+  'bashls',
+  'efm',
+  'elixirls',
+  'gopls',
+  'terraformls',
+  'tailwindcss',
+  'tflint',
+  'tsserver',
+  'pyright',
+  'sumneko_lua',
+  'yamlls',
+  'rust_analyzer',
 }
 
 -- Autoinstall servers
@@ -21,12 +22,11 @@ for _, name in pairs(servers) do
   local server_is_found, server = lsp_installer.get_server(name)
   if server_is_found then
     if not server:is_installed() then
-      print("Installing " .. name)
+      print('Installing ' .. name)
       server:install()
     end
   end
 end
-
 
 local function make_config()
   local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -73,6 +73,12 @@ lsp_installer.on_server_ready(function(server)
     config.filetypes = { 'elixir', 'heex' }
   elseif server.name == 'sumneko_lua' then
     config.settings = require('ai/lua-ls').settings
+  elseif server.name == 'tsserver' then
+    config.on_attach = function(client)
+      -- Disable document formatting; allow efm/prettier to win
+      client.resolved_capabilities.document_formatting = false
+      vim.api.nvim_exec([[set signcolumn=yes]], true)
+    end
   end
   server:setup(config)
 end)
--- .vim/lua/ai/typescript.lua
+++ .vim/lua/ai/typescript.lua
@@ -1,21 +1,8 @@
-local nvim_lsp = require("lspconfig")
-
 _G.lsp_organize_imports = function()
   local params = {
-    command = "_typescript.organizeImports",
+    command = '_typescript.organizeImports',
     arguments = { vim.api.nvim_buf_get_name(0) },
-    title = "",
+    title = '',
   }
   vim.lsp.buf.execute_command(params)
 end
-
-local lsp_shared = require('ai/lsp-shared')
-
-nvim_lsp.tsserver.setup {
-  capabilities = require('ai/lsp-shared').capabilities(),
-  on_attach = function(client)
-    client.resolved_capabilities.document_formatting = false
-    vim.api.nvim_exec([[set signcolumn=yes]], true)
-    lsp_shared.on_attach(client)
-  end,
-}
