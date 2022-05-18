7612c787c5977b0aa2f04b0bf7985e1c53482384
Aiden Scandella <git@sca.ndella.com>
2022-05-18 08:58:19 -0700

    Make autocommands lua; resize equal on vim resize

--- .vim/lua/ai/ui.lua
+++ .vim/lua/ai/ui.lua
@@ -1,2 +1,18 @@
 -- disabled in visual mode
-vim.cmd('au TextYankPost * lua vim.highlight.on_yank { timeout = 250, on_visual = false}')
+local yank_group = vim.api.nvim_create_augroup('ai/yank', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  pattern = '*',
+  group = yank_group,
+  callback = function()
+    vim.highlight.on_yank({ timeout = 250, on_visual = false })
+  end,
+})
+
+local window_group = vim.api.nvim_create_augroup('ai/window', { clear = true })
+vim.api.nvim_create_autocmd('VimResized', {
+  pattern = '*',
+  group = window_group,
+  callback = function()
+    vim.cmd('wincmd =')
+  end,
+})
