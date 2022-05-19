6abd5bfa60d5a8db7c717d26cac1dd04fded9dc9
Aiden Scandella <git@sca.ndella.com>
2022-05-19 08:37:32 -0700

    Clean up some unused modules

--- bootstrap/osx/antigen.zsh
+++
@@ -1,8 +0,0 @@
-ANTIGEN_SOURCE="${DOTFILES}/ext/antigen/antigen.zsh"
-if [[ -s "${ANTIGEN_SOURCE}" ]] ; then
-  . "${ANTIGEN_SOURCE}"
-  antigen bundle Tarrasch/zsh-autoenv
-  antigen apply
-fi
-unset ANTIGEN_SOURCE
-
