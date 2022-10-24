9e020fb48988ea65557298fd663c46e3b2b5e018
Aiden Scandella <git@sca.ndella.com>
2022-10-24 13:27:16 -0700

    Leave leader-a for project search, leader-rc for rust code group

--- .vim/lua/ai/rust-config.lua
+++ .vim/lua/ai/rust-config.lua
@@ -6,7 +6,7 @@ rt.setup({
       -- Hover actions
       vim.keymap.set('n', '<Leader>ra', rt.hover_actions.hover_actions, { buffer = bufnr })
       -- Code action groups
-      vim.keymap.set('n', '<Leader>a', rt.code_action_group.code_action_group, { buffer = bufnr })
+      vim.keymap.set('n', '<Leader>rc', rt.code_action_group.code_action_group, { buffer = bufnr })
       require('ai/lsp-shared').on_attach(client, bufnr)
     end,
   },
