89535999ebb85177f53a693d37f63fe62a5b1d0e
Aiden Scandella <git@sca.ndella.com>
2022-10-25 09:13:24 -0700

    Disable completion in comments

--- .vim/lua/ai/completion.lua
+++ .vim/lua/ai/completion.lua
@@ -54,12 +54,21 @@ tabnine:setup({
 })
 
 cmp.setup({
+  enabled = function()
+    -- disable completion in comments
+    local context = require('cmp.config.context')
+    -- keep command mode completion enabled when cursor is in a comment
+    if vim.api.nvim_get_mode().mode == 'c' then
+      return true
+    else
+      return not context.in_treesitter_capture('comment') and not context.in_syntax_group('Comment')
+    end
+  end,
   experimental = {
     ghost_text = false,
   },
   snippet = {
     expand = function(args)
-      -- For `vsnip` user.
       vim.fn['vsnip#anonymous'](args.body)
     end,
   },
