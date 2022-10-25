d71ba16763abef2c6e77357aa6bfdad773ced351
Aiden Scandella <git@sca.ndella.com>
2022-10-24 21:29:17 -0700

    Make treesitter query prettier

--- .vim/lua/ai/treesitter/classname.lua
+++ .vim/lua/ai/treesitter/classname.lua
@@ -5,8 +5,9 @@ local attribute_query = vim.treesitter.parse_query(
   'tsx',
   [[
   (jsx_element
-    (jsx_opening_element (jsx_attribute
-       (
+    (jsx_opening_element
+      (jsx_attribute
+        (
           (property_identifier) @attr_name)
           (#eq? @attr_name "className")
           (string (string_fragment) @attr_value)
@@ -21,11 +22,11 @@ local attribute_self_closing_query = vim.treesitter.parse_query(
   [[
   (jsx_self_closing_element
     (jsx_attribute
-       (
-          (property_identifier) @attr_name)
-          (#eq? @attr_name "className")
-          (string (string_fragment) @attr_value)
-        )
+      (
+        (property_identifier) @attr_name)
+        (#eq? @attr_name "className")
+        (string (string_fragment) @attr_value)
+      )
     )
   ]]
 )
