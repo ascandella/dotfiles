9675e9b59d897ad81f123d852eb5fc256774f0fb
Aiden Scandella <git@sca.ndella.com>
2022-10-28 08:10:17 -0700

    Make toggleterm horizontal taller

--- .vim/lua/ai/_toggleterm.lua
+++ .vim/lua/ai/_toggleterm.lua
@@ -2,6 +2,13 @@ local function init()
   require('toggleterm').setup({
     direction = 'horizontal',
     open_mapping = [[<A-j>]],
+    size = function(term)
+      if term.direction == 'horizontal' then
+        return 20
+      elseif term.direction == 'vertical' then
+        return vim.o.columns * 0.4
+      end
+    end,
   })
 end
 
