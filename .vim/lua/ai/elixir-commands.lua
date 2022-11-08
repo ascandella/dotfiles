f08a77053c7a24a0b4e217c73e3cb3453fa778b2
Aiden Scandella <git@sca.ndella.com>
2022-11-08 07:54:41 -0800

    Clean up elixir commands with neotest

--- .vim/lua/ai/elixir-commands.lua
+++ .vim/lua/ai/elixir-commands.lua
@@ -1,46 +1,4 @@
-local nvim_lsp = require('lspconfig')
-local Terminal = require('toggleterm.terminal').Terminal
-
 local M = {}
-
-local project_root_finder = nvim_lsp.util.root_pattern('mix.exs', '.git')
-
-local function open_tests(cmd)
-  local buffer_filename = vim.api.nvim_buf_get_name(0)
-  local project_root = project_root_finder(buffer_filename)
-  local term = Terminal:new({
-    cmd = cmd,
-    dir = project_root,
-    close_on_exit = false,
-  })
-  term:open()
-  return term
-end
-
-local function send_command_and_show(command)
-  open_tests(command)
-end
-
-local function current_line_number()
-  local r = vim.api.nvim_win_get_cursor(0)
-  return r[1] - 1
-end
-
-M.run_tests = function()
-  send_command_and_show('mix test --stale')
-end
-
-M.run_test_at_cursor = function()
-  local buffer_filename = vim.api.nvim_buf_get_name(0)
-  local buffer_lineno = current_line_number()
-
-  send_command_and_show('mix test ' .. buffer_filename .. ':' .. buffer_lineno)
-end
-
-M.run_test_at_file = function()
-  local buffer_filename = vim.api.nvim_buf_get_name(0)
-  send_command_and_show('mix test ' .. buffer_filename)
-end
 
 M.copy_module_alias = function()
   vim.api.nvim_feedkeys(
