498df851d9f898071e4fe172480b6dcc5f9f720e
Aiden Scandella <git@sca.ndella.com>
2022-11-10 13:06:31 -0800

    Import polybar config from GitHub: https://github.com/adi1090x/polybar-themes

--- to-install/linux/dotconfig/polybar/launch.sh
+++ to-install/linux/dotconfig/polybar/launch.sh
@@ -1,22 +1,81 @@
 #!/usr/bin/env bash
 
-if pgrep -x polybar ; then
-  pkill -x polybar -USR1 &
-  exit 0
+dir="$HOME/.config/polybar"
+themes=(`ls --hide="launch.sh" $dir`)
+
+launch_bar() {
+	# Terminate already running bar instances
+	killall -q polybar
+
+	# Wait until the processes have been shut down
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+	# Launch the bar
+	if [[ "$style" == "hack" || "$style" == "cuts" ]]; then
+		polybar -q top -c "$dir/$style/config.ini" &
+		polybar -q bottom -c "$dir/$style/config.ini" &
+	elif [[ "$style" == "pwidgets" ]]; then
+		bash "$dir"/pwidgets/launch.sh --main
+	else
+		polybar -q main -c "$dir/$style/config.ini" &	
+	fi
+}
+
+if [[ "$1" == "--material" ]]; then
+	style="material"
+	launch_bar
+
+elif [[ "$1" == "--shades" ]]; then
+	style="shades"
+	launch_bar
+
+elif [[ "$1" == "--hack" ]]; then
+	style="hack"
+	launch_bar
+
+elif [[ "$1" == "--docky" ]]; then
+	style="docky"
+	launch_bar
+
+elif [[ "$1" == "--cuts" ]]; then
+	style="cuts"
+	launch_bar
+
+elif [[ "$1" == "--shapes" ]]; then
+	style="shapes"
+	launch_bar
+
+elif [[ "$1" == "--grayblocks" ]]; then
+	style="grayblocks"
+	launch_bar
+
+elif [[ "$1" == "--blocks" ]]; then
+	style="blocks"
+	launch_bar
+
+elif [[ "$1" == "--colorblocks" ]]; then
+	style="colorblocks"
+	launch_bar
+
+elif [[ "$1" == "--forest" ]]; then
+	style="forest"
+	launch_bar
+
+elif [[ "$1" == "--pwidgets" ]]; then
+	style="pwidgets"
+	launch_bar
+
+elif [[ "$1" == "--panels" ]]; then
+	style="panels"
+	launch_bar
+
+else
+	cat <<- EOF
+	Usage : launch.sh --theme
+		
+	Available Themes :
+	--blocks    --colorblocks    --cuts      --docky
+	--forest    --grayblocks     --hack      --material
+	--panels    --pwidgets       --shades    --shapes
+	EOF
 fi
-
-# Terminate already running bar instances
-killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-MONITOR=""
-# If we're on my Thinkpad, pin Polybar to the builtin display, as opposed to the
-# (possibly-connected) "primary monitor"
-if [ "$(cat /sys/class/drm/card0/card0-eDP-1/status)" = "connected" ] ; then
-  MONITOR="eDP-1"
-fi
-MONITOR="${MONITOR}" polybar ai &
-
-echo "Bars launched..."
