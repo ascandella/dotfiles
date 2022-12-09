0e22538e5094d2907957a3e11d0040dff99e7c71
Aiden Scandella <git@sca.ndella.com>
2022-12-09 11:14:17 -0800

    Fix lsp definition when using pnpm across packages
    Ignore react/index.d.ts

--- .vim/lua/ai/lsp-shared.lua
+++ .vim/lua/ai/lsp-shared.lua
@@ -17,8 +17,35 @@ M.toggle_lsp_formatting = function()
   end
 end
 
+-- From: https://github.com/daliusd/cfg/blob/0e61894c689d736fa8c59ace8f149ecffb187cc4/.vimrc#L319-L332
+--
+local function filter(arr, fn)
+  if type(arr) ~= 'table' then
+    return arr
+  end
+
+  local filtered = {}
+  for k, v in pairs(arr) do
+    if fn(v, k, arr) then
+      table.insert(filtered, v)
+    end
+  end
+
+  return filtered
+end
+
+local function filterReactDTS(value)
+  return string.match(value.filename, 'react/index.d.ts') == nil
+end
+
 local function on_list(options)
-  vim.fn.setqflist({}, ' ', options)
+  -- https://github.com/typescript-language-server/typescript-language-server/issues/216
+  local items = options.items
+  if #items > 1 then
+    items = filter(items, filterReactDTS)
+  end
+
+  vim.fn.setqflist({}, ' ', { title = options.title, items = items, context = options.context })
   vim.api.nvim_command('cfirst')
 end
 
