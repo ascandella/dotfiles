65fc31f9e6d88476a4bb5f37c2c0f310ad227a0e
Aiden Scandella <git@sca.ndella.com>
2023-01-04 10:35:53 -0800

    Remove extra undodir invocation

--- .vim/init.lua
+++ .vim/init.lua
@@ -1,7 +1,5 @@
 -- Use a separate undo directory to support systems with non-bleeding-edge vim
 -- as well
-vim.api.nvim_command('set undodir=~/.vim/undo-nvim/')
-
 require('ai/preferences')
 
 require('ai/_packages')
