f882da66fe2758c26ec9eb2c2fb77cceb69daa54
Aiden Scandella <git@sca.ndella.com>
2023-04-16 12:38:14 -0700

    Move completion symbols to right

--- .vim/lua/ai/completion.lua
+++ .vim/lua/ai/completion.lua
@@ -97,16 +97,15 @@ cmp.setup({
     end,
   },
   formatting = {
-    fields = { 'kind', 'abbr', 'menu' },
+    fields = { 'abbr', 'kind', 'menu' },
     format = lspkind.cmp_format({
-      mode = 'symbol',
+      mode = 'symbol_text',
       -- menu = {
       --   copilot = '[Copilot]',
       --   buffer = '[Buffer]',
       --   nvim_lsp = '[LSP]',
       --   luasnip = '[LuaSnip]',
       --   nvim_lua = '[Lua]',
-      --   latex_symbols = '[Latex]',
       -- },
       maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
       ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
