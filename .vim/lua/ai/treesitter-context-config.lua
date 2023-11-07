22f7e3ebd6304da0347a844419c0258274bad696
Aiden Scandella <git@sca.ndella.com>
2023-11-07 08:22:32 -0800

    Configure treesitter context

--- .vim/lua/ai/treesitter-context-config.lua
+++ .vim/lua/ai/treesitter-context-config.lua
@@ -1,3 +1,6 @@
 require('treesitter-context').setup({
   enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+  max_lines = 12,
+  line_numbers = false,
+  separator = '-',
 })
