e41b3bce119dd2b6a714b5ea3fc0223f75ce34b4
Aiden Scandella <git@sca.ndella.com>
2024-01-23 21:26:03 -0800

    Update macOS bootstrap packages

--- bootstrap/osx/brew-basics.zsh
+++ bootstrap/osx/brew-basics.zsh
@@ -1,5 +1,5 @@
 _install_brew_packages () {
-  local pkgs=(git htop tmux ccat ctags the_silver_searcher)
+  local pkgs=(git htop tmux bat ripgrep)
   for pkg in "${pkgs[@]}" ; do
     brew install "${pkg}" || brew upgrade "${pkg}"
   done
