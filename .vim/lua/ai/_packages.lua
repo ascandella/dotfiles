69f91ce38ab85bbb86368e1a9092efd77a57bf79
Aiden Scandella <git@sca.ndella.com>
2024-02-06 10:08:37 -0800

    Add vim-kdl

--- .vim/lua/ai/_packages.lua
+++ .vim/lua/ai/_packages.lua
@@ -295,6 +295,8 @@ local packages = {
 
   -- Astro syntax
   { 'wuelnerdotexe/vim-astro', ft = 'astro' },
+  -- KDL syntax, for zellij
+  { 'imsnif/kdl.vim', ft = 'kdl' },
   --
   -- Start screen
   {
