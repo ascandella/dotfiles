4e34d982a1c08269392d9b95c9c0669d9de42ff6
Aiden Scandella <git@sca.ndella.com>
2024-02-09 17:51:07 -0800

    vim: Enable triple-backtick endwise everywhere; disable single

--- .vim/lua/ai/_autopairs.lua
+++ .vim/lua/ai/_autopairs.lua
@@ -1,5 +1,6 @@
 local npairs = require('nvim-autopairs')
 local Rule = require('nvim-autopairs.rule')
+local cond = require('nvim-autopairs.conds')
 
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
@@ -13,6 +14,12 @@ npairs.setup({
 npairs.add_rules({
   Rule('"""', '"""', { 'elixir', 'eelixir' }),
 })
+
+npairs.remove_rule('`')
+local backtick_rule = npairs.get_rule('```')
+npairs.remove_rule('```')
+backtick_rule.filetypes = nil
+npairs.add_rules({ backtick_rule })
 
 npairs.add_rules(require('nvim-autopairs.rules.endwise-elixir'))
 npairs.add_rules(require('nvim-autopairs.rules.endwise-lua'))
