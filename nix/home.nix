9e8d0a126ac506ff0476082071fc72a9317436fd
Aiden Scandella <git@sca.ndella.com>
2024-03-13 08:10:57 -0700

    nix: fix AWS CLI version

--- nix/home.nix
+++ nix/home.nix
@@ -23,7 +23,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    awscli
+    awscli2
     direnv
     htop
     neovim
