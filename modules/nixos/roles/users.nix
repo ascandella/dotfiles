5c8137852a32724bb092162820823715b60a152c
Aiden Scandella <git@sca.ndella.com>
2024-03-13 19:40:19 -0700

    nix: Cleanup

--- modules/nixos/roles/users.nix
+++ modules/nixos/roles/users.nix
@@ -18,7 +18,7 @@
     description = "Aiden";
     shell = pkgs.zsh;
     extraGroups = [ "networkmanager" "wheel" "media" ];
-    packages = with pkgs; [];
+    # TODO: put these somewhere central
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG8n0CileVU4jKA43cCTD/zHeg2Ozp+JX0qW80P/7iau aiden@ai-studio"
       "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBM4ODzK94AfFpbIsx2I+s3TsrDX6LcQTDcxPfX037k6DUANnNNcUtP1Gl7l9WTm9P59l2iQL9R9hXIfGi6nmHOg= Personal@secretive.Aiden’s-MacBook-Pro.local"
