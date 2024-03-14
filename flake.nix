96dc9a48acc5fab805184ecfac81d808be79c70e
Aiden Scandella <git@sca.ndella.com>
2024-03-13 18:43:57 -0700

    Use unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
