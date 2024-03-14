7d5e59f3c437ec1327b83e2b7ea376d7b13f5f7d
Aiden Scandella <git@sca.ndella.com>
2024-03-13 17:49:59 -0700

    mac: add some system defaults

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -2,5 +2,27 @@
 {
   config = lib.mkIf pkgs.stdenv.isDarwin {
     services.nix-daemon.enable = true;
+
+    system = {
+      defaults = {
+        NSGlobalDomain = {
+          # Set to dark mode
+          AppleInterfaceStyle = "Dark";
+
+          # Set a fast key repeat rate
+          KeyRepeat = 2;
+
+          # Shorten delay before key repeat begins
+          InitialKeyRepeat = 12;
+        };
+
+        # Disable "Are you sure you want to open" dialog
+        LaunchServices.LSQuarantine = false;
+        dock = {
+          # Automatically show and hide the dock
+          autohide = true;
+        };
+      };
+    };
   };
 }
