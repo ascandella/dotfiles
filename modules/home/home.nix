ccba29a736da7e78486077a600fe2d8b643ad1ab
Aiden Scandella <git@sca.ndella.com>
2024-03-13 21:10:03 -0700

    Cleanup

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -60,22 +60,6 @@
     reload-home-manager-config = "home-manager switch && exec zsh -L";
   };
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # ".config/home-manager".source = config.lib.file.mkOutOfStoreSymlink ./.;
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. If you don't want to manage your shell through Home
   # Manager then you have to manually source 'hm-session-vars.sh' located at
