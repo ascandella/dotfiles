019c662dab2d52a3f7f1a9851d7933b3b8b72258
Aiden Scandella <git@sca.ndella.com>
2024-03-13 20:50:36 -0700

    Move files to proper location

--- modules/home/dotconfig.nix
+++ modules/home/dotconfig.nix
@@ -9,11 +9,12 @@
     "yazi".source = ./files/yazi;
     "nvim".source = ./files/nvim;
     "zellij".source = ./files/zellij;
-    ".ignore".source = ./files/ignore;
-    ".pylintrc".source = ./files/.pylintrc;
-    ".stylua.toml".source = ./files/.stylua.toml;
   };
+
   home.file = lib.mkIf pkgs.stdenv.isDarwin {
     "Library/Application Support/Übersicht/widgets/simple-bar".source = inputs.simple-bar-src.outPath;
+    ".ignore".source = ./files/ignore;
+    ".pylintrc".source = ./files/.pylintrc;
+    ".luacheckrc".source = ./files/.luacheckrc;
   };
 }
