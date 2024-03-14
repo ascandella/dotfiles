ac1c4ec3b13b28b1caf170d98d6e6f2b15346846
Aiden Scandella <git@sca.ndella.com>
2024-03-13 20:41:57 -0700

    Cleanup: remove tmux

--- .tmux/README.md
+++
@@ -1,7 +0,0 @@
-## Terminfo
-
-This fixes italics under tmux.
-
-To add `tmux-256color` as a valid `$TERM` config:
-
-tic -o ~/.terminfo tmux-256color.terminfo.txt
--- .tmux/battery-ify.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-for cmd in pmset upower acpi ; do
-  if command -v "$cmd" > /dev/null ; then
-    tmux set -g @plugin 'tmux-plugins/tmux-battery'
-    break
-  fi
-done
