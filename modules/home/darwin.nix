a48ec6cd73eeaa75b5f66d080d58367e5f432008
Aiden Scandella <git@sca.ndella.com>
2024-03-13 21:42:47 -0700

    sigh: add jira

--- modules/home/darwin.nix
+++ modules/home/darwin.nix
@@ -2,6 +2,10 @@
 {
   config = lib.mkIf pkgs.stdenv.isDarwin {
     home.sessionPath = ["/opt/homebrew/bin/"];
+    home.packages = with pkgs; [
+      jira-cli-go
+    ];
+
     programs.zsh.initExtra = ''
       # NOT related to homebrew, this is stuff like docker
       export PATH="$PATH:/usr/local/bin"
@@ -12,10 +16,14 @@
       unset _iterm2_shell
     '';
 
-    # TODO: remove these, I'm not using them
     programs.zsh.shellAliases = {
+      # TODO: remove these, I'm not using them
       topbar = "yabai -m config external_bar all:38:0";
       bottombar = "yabai -m config external_bar all:0:30";
+
+      # TODO: not currently installing
+      jiraissues = "jira issue list -a$(jira me)";
+      inprog = "jiraissues -s 'In Progress' --plain --columns KEY,SUMMARY --no-headers";
     };
   };
 }
