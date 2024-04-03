fd3179e15f86c9af141836c29462955e47883b2a
Aiden Scandella <git@sca.ndella.com>
2024-04-03 16:27:44 -0700

    add gitea (#18)
    * add gitea
    
    * allow firewall
    
    * remove firewall

---
+++ modules/nixos/roles/gitea.nix
@@ -0,0 +1,29 @@
+{ config, ... }:
+
+{
+  age.secrets.gitea-db-pass = {
+    file = ../../../secrets/gitea-db-pass.age;
+    owner = config.services.gitea.user;
+    mode = "0400";
+  };
+
+  services = {
+    gitea = {
+      enable = true;
+      database = {
+        type = "mysql";
+        passwordFile = config.age.secrets.gitea-db-pass.path;
+      };
+      settings = {
+        server = {
+          HTTP_PORT = 3009;
+          DOMAIN = "code.ndella.com";
+          SSH_PORT = 9023;
+          ROOT_URL = "https://code.ndella.com/";
+        };
+      };
+    };
+
+    mysqlBackup = { databases = [ "gitea" ]; };
+  };
+}
