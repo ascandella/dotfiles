711e442c418c8f50ce199d9c4a01f649cebc643f
Aiden Scandella <git@sca.ndella.com>
2024-04-04 17:51:12 -0700

    fix nextcloud systemd after

--- modules/nixos/roles/nextcloud.nix
+++ modules/nixos/roles/nextcloud.nix
@@ -57,8 +57,8 @@
         }
       ];
     };
-  };
-  systemd.services.nextcloud-setup = {
-    after = [ "mysql.service" ];
+    systemd.services.nextcloud-setup = {
+      after = [ "mysql.service" ];
+    };
   };
 }
