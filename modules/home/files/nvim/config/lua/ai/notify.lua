224facecb5addca6ba0688e223a6764132a77e98
Aiden Scandella <git@sca.ndella.com>
2024-07-31 09:19:09 -0700

    vim: disable superfluous lspsaga hover messages

--- modules/home/files/nvim/config/lua/ai/notify.lua
+++ modules/home/files/nvim/config/lua/ai/notify.lua
@@ -4,4 +4,17 @@ notify.setup({
   render = 'wrapped-compact',
 })
 
-vim.notify = notify
+local banned_messages = {
+  -- https://github.com/nvimdev/lspsaga.nvim/issues/1295
+  -- https://github.com/rcarriga/nvim-notify/issues/114#issuecomment-1179754969
+  'No information available',
+}
+
+vim.notify = function(msg, ...)
+  for _, banned in ipairs(banned_messages) do
+    if msg == banned then
+      return
+    end
+  end
+  notify(msg, ...)
+end
