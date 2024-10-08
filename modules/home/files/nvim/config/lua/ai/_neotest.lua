d2a726b68088cf8f71f50ee9c824f968834ac177
Aiden Scandella <git@sca.ndella.com>
2024-10-08 15:14:24 -0700

    neovim: add descriptions for neotest mappings

--- modules/home/files/nvim/config/lua/ai/_neotest.lua
+++ modules/home/files/nvim/config/lua/ai/_neotest.lua
@@ -13,19 +13,27 @@ local function init()
       running = '◴',
       skipped = 'ﰸ',
     },
+    diagnostic = {
+      enabled = true,
+      severity = 1,
+    },
+    output = {
+      open_on_run = true,
+      enter = false,
+    },
   })
 
   -- Test running (neotest)
-  vim.keymap.set('n', '<Leader>eu', neotest.run.run, { silent = true })
+  vim.keymap.set('n', '<Leader>eu', neotest.run.run, { silent = true, desc = 'Nearest test' })
   vim.keymap.set('n', '<Leader>ee', function()
     neotest.run.run(vim.fn.expand('%'))
-  end, { silent = true })
+  end, { silent = true, desc = 'Test file' })
   vim.keymap.set('n', '<Leader>ei', function()
     neotest.output.open()
-  end, { silent = true })
+  end, { silent = true, desc = 'Open test output' })
   vim.keymap.set('n', '<Leader>es', function()
     neotest.summary.toggle()
-  end, { silent = true })
+  end, { silent = true, desc = 'Toggle test summary' })
 end
 
 return {
