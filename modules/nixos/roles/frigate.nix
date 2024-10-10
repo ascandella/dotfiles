cbddcd15c3667a5eb6481b9e0edf8b94cbe5fbea
Aiden Scandella <git@sca.ndella.com>
2024-10-10 16:30:53 -0700

    frigate: upgrade cuda packages

--- modules/nixos/roles/frigate.nix
+++ modules/nixos/roles/frigate.nix
@@ -52,22 +52,17 @@ with lib;
           # Make sure this is the same as what's in system configuration
           "${config.hardware.nvidia.package}/lib/libcuda.so:/usr/lib/libcuda.so:ro"
         ]
-        ++ map (soFile: "${lib.getLib pkgs.cudaPackages_11.cuda_nvrtc}/lib/${soFile}:/usr/lib/${soFile}:ro")
-          [
-            "libnvrtc.so"
-          ]
-        ++
-          map (soFile: "${lib.getLib pkgs.cudaPackages_11.cudnn_8_9}/lib/${soFile}:/usr/lib/${soFile}:ro")
-            [
-              "libcudnn_cnn_infer.so.8"
-              "libcudnn_ops_infer.so.8"
-            ]
-        ++
-          map (soFile: "${lib.getLib pkgs.cudaPackages_11.libcublas}/lib/${soFile}:/usr/lib/${soFile}:ro")
-            [
-              "libcublas.so"
-              "libcublasLt.so"
-            ];
+        ++ map (soFile: "${lib.getLib pkgs.cudaPackages.cuda_nvrtc}/lib/${soFile}:/usr/lib/${soFile}:ro") [
+          "libnvrtc.so"
+        ]
+        ++ map (soFile: "${lib.getLib pkgs.cudaPackages.cudnn_9_3}/lib/${soFile}:/usr/lib/${soFile}:ro") [
+          "libcudnn_cnn.so.9"
+          "libcudnn_ops.so.9"
+        ]
+        ++ map (soFile: "${lib.getLib pkgs.cudaPackages.libcublas}/lib/${soFile}:/usr/lib/${soFile}:ro") [
+          "libcublas.so"
+          "libcublasLt.so"
+        ];
       extraOptions = [
         # Add GPU
         "--device"
