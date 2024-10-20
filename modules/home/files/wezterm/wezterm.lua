dba0ae2fc345c8d43681586e869a609bc637e16d
Aiden Scandella <git@sca.ndella.com>
2024-10-20 09:31:15 -0700

    zellij: add 3up vertical

--- modules/home/files/wezterm/wezterm.lua
+++ modules/home/files/wezterm/wezterm.lua
@@ -82,14 +82,14 @@ wezterm.plugin.require('https://github.com/nekowinston/wezterm-bar').apply_to_co
     },
   },
   tabs = {
-    numerals = 'arabic',        -- or "roman"
+    numerals = 'arabic', -- or "roman"
     pane_count = 'superscript', -- or "subscript", false
     brackets = {
       active = { '', ':' },
       inactive = { '', ':' },
     },
   },
-  clock = {           -- note that this overrides the whole set_right_status
+  clock = { -- note that this overrides the whole set_right_status
     enabled = true,
     format = '%H:%M', -- use https://wezfurlong.org/wezterm/config/lua/wezterm.time/Time/format.html
   },
