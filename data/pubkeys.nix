99a6fb3200ff8ed61a6feebf8fa315c7074f7553
Aiden Scandella <git@sca.ndella.com>
2024-11-17 23:20:11 -0800

    fmt

--- data/pubkeys.nix
+++ data/pubkeys.nix
@@ -1,7 +1,6 @@
 let
   studioKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG8n0CileVU4jKA43cCTD/zHeg2Ozp+JX0qW80P/7iau aiden@ai-studio";
   user = [
-
     studioKey
     "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBM4ODzK94AfFpbIsx2I+s3TsrDX6LcQTDcxPfX037k6DUANnNNcUtP1Gl7l9WTm9P59l2iQL9R9hXIfGi6nmHOg= Personal@secretive.Aiden’s-MacBook-Pro.local"
   ];
