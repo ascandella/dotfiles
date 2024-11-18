5b1c6915844e007e276cc6957d8a943323cdf3c2
Aiden Scandella <git@sca.ndella.com>
2024-11-17 23:16:52 -0800

    add age for manual encryption

--- data/pubkeys.nix
+++ data/pubkeys.nix
@@ -1,8 +1,14 @@
 let
+  studioKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG8n0CileVU4jKA43cCTD/zHeg2Ozp+JX0qW80P/7iau aiden@ai-studio";
   user = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG8n0CileVU4jKA43cCTD/zHeg2Ozp+JX0qW80P/7iau aiden@ai-studio"
+
+    studioKey
     "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBM4ODzK94AfFpbIsx2I+s3TsrDX6LcQTDcxPfX037k6DUANnNNcUtP1Gl7l9WTm9P59l2iQL9R9hXIfGi6nmHOg= Personal@secretive.Aiden’s-MacBook-Pro.local"
   ];
+  age = {
+    studio = studioKey;
+    workbook = "age1luaq2rfyylnp93950l4u0rsfd0kv4dh2xagr47kuc7jr3640z96q04f5ga";
+  };
   hosts = {
     baymax = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHvV4lf5yReZHdlE2qVnc7gVrigfx2ged272S5kBy+D3";
   };
@@ -10,7 +16,7 @@ let
 in
 {
   aispace = {
-    inherit user hosts;
+    inherit user hosts age;
     ageHostKeys = host: ageCompatibleKeys ++ [ hosts.${host} ];
   };
 }
--- modules/home/home.nix
+++ modules/home/home.nix
@@ -67,6 +67,7 @@
     # The home.packages option allows you to install Nix packages into your
     # environment.
     home.packages = with pkgs; [
+      age # encryption
       argocd # manage argo
       awscli2
       bat
