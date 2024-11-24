4a52668d5b5cc854fcfd5e8ab9f3feef9c9e8407
Aiden Scandella <git@sca.ndella.com>
2024-11-23 23:59:58 -0800

    vector: open firewall for prometheus exporter

--- modules/nixos/roles/logging.nix
+++ modules/nixos/roles/logging.nix
@@ -30,6 +30,7 @@ with lib;
   };
 
   config = mkIf cfg.vector.enable {
+    networking.firewall = mkIf cfg.openFirewall { allowedTCPPorts = [ 9598 ]; };
     services = {
       vector = {
         enable = true;
