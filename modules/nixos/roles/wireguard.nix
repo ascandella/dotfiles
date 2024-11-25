ae20c02cb0902fc1c22c0da76cca132ab0496568
Aiden Scandella <git@sca.ndella.com>
2024-11-24 16:00:21 -0800

    more fixes

--- modules/nixos/roles/wireguard.nix
+++ modules/nixos/roles/wireguard.nix
@@ -2,11 +2,13 @@
 
 with lib;
 {
-  options = mkOption {
-    services.aispace.wireguard = mkOption {
-      type = types.str;
-      default = "wg0";
-      description = "Wireguard interface to configure";
+  options = {
+    services.aispace.wireguard = {
+      interface = mkOption {
+        type = types.str;
+        default = "wg0";
+        description = "Wireguard interface to configure";
+      };
     };
   };
 }
