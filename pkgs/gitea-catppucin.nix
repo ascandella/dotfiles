aed6440e13e45cd81d25b91657fe2aca9c3a268e
Aiden Scandella <git@sca.ndella.com>
2024-12-07 11:30:44 -0800

    fixes

--- pkgs/gitea-catppucin.nix
+++ pkgs/gitea-catppucin.nix
@@ -1,12 +1,12 @@
-{ stdenv, fetchzip, ... }:
+{ stdenv, ... }:
 
 stdenv.mkDerivation rec {
   pname = "gitea-catppucin";
   version = "1.0.1";
 
-  src = fetchzip {
+  src = fetchTarball {
     url = "https://github.com/catppuccin/gitea/releases/download/v${version}/catppuccin-gitea.tar.gz";
-    sha256 = "bJyI7RvVCf0M5vs8Qi+uAHv74CWxSDZ0Bb6zWJ4x4CM=";
+    sha256 = "1qpy39j9an1b78kgy2rwpf49wkxv90jxq422f27bh8yj1nw6bpks";
   };
 
   buildPhase = "";
