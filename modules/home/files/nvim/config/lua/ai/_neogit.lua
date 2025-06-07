b48048175e782cb475486cdc938ebfeda66c8008
Aiden Scandella <git@sca.ndella.com>
2025-06-06 17:26:41 -0700

    vim: add leader-gc gitlab open PR support

--- modules/home/files/nvim/config/lua/ai/_neogit.lua
+++ modules/home/files/nvim/config/lua/ai/_neogit.lua
@@ -32,16 +32,25 @@ local M = {}
 M.open_pr = function()
   a.run(function()
     local remote_url = neogit.cli.config.get('remote.origin.url').call().stdout[1]
-    local repo_name = remote_url:gsub('^git@github.com:', '')
-    local clean_name = repo_name:gsub('.git$', '')
     local branch = require('neogit.lib.git.branch').current()
-    print(vim.inspect(branch))
+    local repo_name
+    local code_forge = 'github.com'
+    local pull_url = 'NOTIMPLEMENTED'
+    if string.find(remote_url, 'github.com') then
+      repo_name = remote_url:gsub('^git@github.com:', '')
+      pull_url = string.format('pull/new/%s', branch)
+    elseif string.find(remote_url, 'gitlab.com') then
+      repo_name = remote_url:gsub('^git@gitlab.com:', '')
+      code_forge = 'gitlab.com'
+      pull_url = string.format('merge_requests/new?merge_request[source_branch]=%s', branch)
+    end
+    local clean_name = repo_name:gsub('.git$', '')
 
-    local open_pr_url = string.format('https://github.com/%s/pull/new/%s', clean_name, branch)
+    local open_pr_url = string.format('https://%s/%s/%s', code_forge, clean_name, pull_url)
     a.util.scheduler()
 
     if vim.fn.has('mac') == 1 then
-      vim.cmd('silent !open ' .. open_pr_url)
+      vim.cmd('silent !open "' .. open_pr_url .. '"')
     elseif vim.fn.executable('xdg-open') == 1 then
       vim.cmd('silent !xdg-open ' .. open_pr_url)
     else
