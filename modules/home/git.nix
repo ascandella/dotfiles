162256a6f3726d05ef42cee87742d27bc1d09eb8
Aiden Scandella <git@sca.ndella.com>
2025-09-16 17:26:31 -0700

    gh: remove conflicting completion

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -151,15 +151,6 @@
       \git branch -D $(_gb)
     }
 
-    _gh() {
-      is_in_git_repo || return
-      git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
-      fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
-        --header 'Press CTRL-S to toggle sort' \
-        --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES |
-      grep -o "[a-f0-9]\{7,\}"
-    }
-
     _gr() {
       is_in_git_repo || return
       git remote -v | awk '{print $1 "\t" $2}' | uniq |
