07bc887c4039e41e7fdd3bd666cf2984ddb028f2
Aiden Scandella <git@sca.ndella.com>
2026-03-21 07:08:25 -0700

    hash

--- modules/plex/default.nix
+++ modules/plex/default.nix
@@ -12,11 +12,10 @@
     let
       plexpass = pkgs.plex.override {
         plexRaw = pkgs.plexRaw.overrideAttrs (_old: rec {
-          # https://downloads.plex.tv/plex-media-server-new/1.43.0.10492-121068a07/debian/plexmediaserver_1.43.0.10492-121068a07_amd64.deb
           version = "1.43.0.10492-121068a07";
           src = pkgs.fetchurl {
             url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-            sha256 = "sha256-4ZbSGQGdkXCCZZ00w0/BwRHju4DJUQQBGid0gBFK0Ck=";
+            sha256 = "sha256-HA779rkjy8QBlW2+IsRmgu4t5PT2Gy0oaqcJm+9zCYE=";
           };
         });
       };
