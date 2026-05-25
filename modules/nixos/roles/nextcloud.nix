6d839ed5eb08bf333bcdd90b5aa42edcad6787ad
Aiden Scandella <git@sca.ndella.com>
2026-05-25 10:10:11 -0700

    re-enable mysql

---
+++ modules/nixos/roles/nextcloud.nix
@@ -0,0 +1,40 @@
+{ config, pkgs, ... }:
+
+{
+  config = {
+    age.secrets.nextcloud-db-pass = {
+      owner = "nextcloud";
+      group = "nextcloud";
+      file = ../../../secrets/nextcloud-db-pass.age;
+      mode = "0400";
+      path = "/etc/nextcloud/db-pass";
+    };
+
+    services = {
+      mysqlBackup = {
+        enable = true;
+        databases = [ "nextcloud" ];
+      };
+      mysql = {
+        enable = true;
+        ensureDatabases = [ "nextcloud" ];
+        package = pkgs.mysql80;
+        ensureUsers = [
+          {
+            name = "nextcloud";
+            ensurePermissions = {
+              "nextcloud.*" = "ALL PRIVILEGES";
+            };
+          }
+        ];
+        settings = {
+          mysqld = {
+            # Bug in nextcloud 30
+            # https://github.com/nextcloud/server/issues/47275#issuecomment-2360521770
+            sql_mode = "STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION";
+          };
+        };
+      };
+    };
+  };
+}
