8ee72dcd8999dc9b9573b5fa19b2f89fcfb793a7
Aiden Scandella <git@sca.ndella.com>
2026-06-06 14:21:33 -0700

    vuetorrent: 2.25 to 2.34

--- pkgs/vuetorrent.nix
+++ pkgs/vuetorrent.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "vuetorrent";
-  version = "2.25.0";
+  version = "2.34.0";
 
   src = fetchzip {
     url = "https://github.com/WDaan/VueTorrent/releases/download/v${version}/vuetorrent.zip";
-    sha256 = "sOaQNw6AnpwNFEextgTnsjEOfpl3/lpoOZFgFOz7Bos=";
+    sha256 = "sha256-MtTN4O1sCF7JhSzz218qrF+zNZEII09AhLxG6fCPIOk=";
   };
 
   buildPhase = "";
