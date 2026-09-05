5068b93218f4219e967dd9b4b4284cd1b2ae1ae4
Aiden Scandella <git@sca.ndella.com>
2026-09-04 19:10:17 -0700

    plex: update 1.43.0 -> 1.43.3

--- modules/plex/default.nix
+++ modules/plex/default.nix
@@ -12,7 +12,7 @@
     let
       plexpass = pkgs.plex.override {
         plexRaw = pkgs.plexRaw.overrideAttrs (_old: rec {
-          version = "1.43.0.10492-121068a07";
+          version = "1.43.3.10896-cb3ebc72d";
           src = pkgs.fetchurl {
             url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
             sha256 = "sha256-HA779rkjy8QBlW2+IsRmgu4t5PT2Gy0oaqcJm+9zCYE=";
