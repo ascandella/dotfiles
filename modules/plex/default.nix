d4f51a80ab39e4144413b8fcdcd493b19d6faa9d
Aiden Scandella <git@sca.ndella.com>
2026-09-04 19:12:03 -0700

    plex: update sha

--- modules/plex/default.nix
+++ modules/plex/default.nix
@@ -15,7 +15,7 @@
           version = "1.43.3.10896-cb3ebc72d";
           src = pkgs.fetchurl {
             url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-            sha256 = "sha256-HA779rkjy8QBlW2+IsRmgu4t5PT2Gy0oaqcJm+9zCYE=";
+            sha256 = "sha256-qgnyZt3PQI4Qz3ulYbbkVObhCbqUFjlraWW9THnzcUk=";
           };
         });
       };
